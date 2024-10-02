c8dba2eb58fb3b0ad38b60cf8250756ebe408e4e
kumarjhamishra <03satyam05@gmail.com>
2024-10-02 18:54:24 +0530

    first code

---
+++ .vscode/Halloumi_boxes_800.cpp
@@ -0,0 +1,26 @@
+#inlcude <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+
+        if(k >= 2) cout<<"YES";
+        else{
+            if(is_sorted(arr.begin(), aee.end())) cout<<"YES";
+            else cout<<"NO";
+        }
+    }
+    return 0;
+}
