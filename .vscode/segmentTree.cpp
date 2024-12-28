d357e5275c6e3bcaaefb67634dca5981ed8a333f
kumarjhamishra <03satyam05@gmail.com>
2024-12-28 16:37:16 +0530

    update range code added

--- .vscode/segmentTree.cpp
+++ .vscode/segmentTree.cpp
@@ -2,7 +2,7 @@ class NumArray {
 public:
     int n;
     vector<int> segTree, nums;
-    //vector<int> lazy; // for update function
+    vector<int> lazy; // for update function
 
     void build(int seg_i, int l, int r, vector<int> &nums){
         // base case
@@ -26,7 +26,7 @@ public:
         }
 
         int mid = l + (r-l)/2;
-        if(mid <= index) updateSegTree(2*seg_i+1, l, mid, index, val);
+        if(index <= mid) updateSegTree(2*seg_i+1, l, mid, index, val);
         else updateSegTree(2*seg_i+2, mid+1, r, index, val);
 
         segTree[seg_i] = segTree[2*seg_i+1] + segTree[2*seg_i+2];
@@ -45,11 +45,56 @@ public:
         return querySegmentTree(2*seg_i+1, s, mid, qs, qe) + querySegmentTree(2*seg_i+2, mid+1, e, qs, qe);
     }
 
+    void updateSegmentRange(int qs, int qe, int seg_i, int l, int r, int value){
+        // if for the curr segment node we have corresponding lazy value
+        // then update the segment value and give it's child the lazy value
+        if(lazy[seg_i] != 0){
+            // this curr node represents the (r-l+1) nodes in the nums so...
+            segTree[seg_i] += (r-l+1)*value;
+            // if it's not a leaf node give it's child the lazy value
+            if(l != r){
+                lazy[2*seg_i+1] += value; // left 
+                lazy[2*seg_i+2] += value; // right
+            }
+            // now we have used the lazy value so reset it to 0
+            lazy[seg_i] = 0;
+        }
+
+        // the range represented by [l, r] is out of range of [qs, qe]
+        if(qe < l || r < qs){
+            // do nothing
+            return ;
+        }
+
+        // curr [l, r] is totally in [qs, qe]
+        if(qs <= l && r <= qe){
+            // update the node with the value
+            // curr segment node represents the nodes (r-l+1)
+            segTree[seg_i] += (r-l+1)*value;
+            // just give it's child the value and don't update them now
+            if(l != r){
+                lazy[2*seg_i+1] += value;
+                lazy[2*seg_i+2] += value;
+            }
+
+            return ;
+        }
+
+        // overlapping ranges
+        int mid = l + (r-l)/2;
+        updateSegmentRange(qs, qe, 2*seg_i+1, l, mid, value);
+        updateSegmentRange(qs, qe, 2*seg_i+2, mid+1, r, value);
+
+        // after coming from recursion update the curr segment node
+        segTree[seg_i] = segTree[2*seg_i+1] + segTree[2*seg_i+2];
+        return ;
+    }
+
     NumArray(vector<int>& nums) {
-        nums = nums;
+        this->nums = nums;
         n = nums.size();
         segTree.resize(4*n);
-        //lazy.resize(4*n, 0); // the lazy arr is need to be initialised with value 0
+        lazy.resize(4*n, 0); // the lazy arr is need to be initialised with value 0
         build(0, 0, n-1, nums);
     }
     
@@ -59,6 +104,12 @@ public:
     
     int sumRange(int left, int right) {
         return querySegmentTree(0, 0, n-1, left, right);
+    }
+
+    // code for update range of segment tree
+    void updateRange(int qs, int qe, int value){
+        // lazy propagation
+        updateSegmentRange(qs, qe, 0, 0, n-1, value);
     }
 };
 
