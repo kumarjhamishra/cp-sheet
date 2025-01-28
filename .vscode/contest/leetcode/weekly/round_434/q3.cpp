785242dbe6b9e045e41b87bb64972238dded7ca4
kumarjhamishra <03satyam05@gmail.com>
2025-01-28 12:48:29 +0530

    q3

--- .vscode/contest/leetcode/weekly/round_434/q3.cpp
+++ .vscode/contest/leetcode/weekly/round_434/q3.cpp
@@ -4,41 +4,30 @@ using namespace std;
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        int final_max_freq = INT_MIN, freq_of_k = 0;
-
-        // count the total frequecy of k in nums
         int n = nums.size();
-
-        // mark start and end index of each unique element and also their total frequency and 
-        // also the frequency of k till each index
-        vector<int> start(51, -1), freqK(n, 0), end(51, -1), freq(51, 0);
-        for(int i = 0; i < n; i++){
-            if(start[nums[i]] == -1) start[nums[i]] = i;
-            end[nums[i]] = i;
-            freq[nums[i]]++;
-            if(nums[i] == k){
-                if(i == 0) freqK[i] = 1;
-                else freqK[i] = freqK[i-1] + 1;
-            }
-        }
-
-        freq_of_k = freqK[n-1];
-        for(int i = 1; i <= 50; i++){
-            int start_i = start[i];
-
-            // means this element is not present
-            if(start_i == -1) continue;
-
-            int end_i = end[i];
-
-            // total k which comes in the inside the subarray of number i
-            int curr_freq_k = freqK[end_i] - (start_i != 0 ? freqK[start_i-1] : 0);
-
-            final_max_freq = max(final_max_freq, freq_of_k - curr_freq_k + freq[i]);
-        }
-
-        return final_max_freq;
         
+        vector<int> freq(51, 0);
+        for(int i : nums) freq[i]++;
+
+        int maxFreq = INT_MIN;
+        for(int num = 1; num <= 50; num++){
+            if(freq[num] == 0) continue;
+
+            // modified kadane's also
+            int maxSubarrySum = 0, currSum = 0;
+            for(int i = 0; i < n; i++){
+                // if it's k treat it a element whose value is -1
+                if(nums[i] == k) currSum--;
+                // if this nums[i] is that number num for which we are checking then treat it as a element of value 1
+                if(nums[i] == num) currSum++;
+
+                currSum = max(0, currSum);
+                maxSubarrySum = max(maxSubarrySum, currSum);
+            }
+            maxFreq = max(maxFreq, maxSubarrySum);
+        }
+
+        return freq[k] + maxFreq;
     }
 };
 
